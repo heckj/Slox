2294a0014ae17109010be63a79565d926c48b273
Joe Heck <heckj@mac.com>
2021-03-02 17:46:32 -0800

    swiftformat enabled

--- Slox/Lox.swift
+++ Slox/Lox.swift
@@ -5,30 +5,30 @@
 //  Created by Joseph Heck on 3/2/21.
 //
 
-import Foundation
 import Darwin
+import Foundation
 
 class Lox {
     static var hadError: Bool = false
     public static func main(args: [String]) throws {
-        if (args.count > 1) {
+        if args.count > 1 {
             print("Usage: slox [script]")
             exit(64)
-        } else if (args.count == 1) {
+        } else if args.count == 1 {
             try runFile(args[0])
         } else {
             try runPrompt()
         }
     }
-    
+
     // translated through https://craftinginterpreters.com/scanning.html#token-type
-    
+
     static func runFile(_ path: String) throws {
 //        byte[] bytes = Files.readAllBytes(Paths.get(path));
 //        run(new String(bytes, Charset.defaultCharset()));
         let contents = try String(contentsOfFile: path, encoding: .utf8)
         run(contents)
-        if (hadError) {
+        if hadError {
             exit(65)
         }
     }
@@ -43,15 +43,15 @@ class Lox {
 //          if (line == null) break;
 //          run(line);
 //        }
-        while(true) {
-            print("> ", terminator:"")
+        while true {
+            print("> ", terminator: "")
             if let interactiveString = readLine(strippingNewline: true) {
                 run(interactiveString)
                 hadError = false
             }
         }
     }
-    
+
     static func run(_ source: String) {
 //        List<Token> tokens = scanner.scanTokens();
 //
@@ -64,7 +64,7 @@ class Lox {
             print(token)
         }
     }
-    
+
     public static func error(_ line: Int, message: String) {
         report(line: line, example: "", message: message)
     }
--- Slox/main.swift
+++ Slox/main.swift
@@ -8,4 +8,3 @@
 import Foundation
 
 print("Hello, World!")
-
