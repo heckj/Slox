6a163576d66d0ab10dd22c884ce75e06ec8e1191
Joe Heck <heckj@mac.com>
2021-03-03 19:46:09 -0800

    section 4.2.1 - token type

--- Slox/Lox.swift
+++ Slox/Lox.swift
@@ -23,6 +23,34 @@ class Lox {
 
     // translated through https://craftinginterpreters.com/scanning.html#token-type
 
+    enum TokenType {
+        
+        // single-character tokens
+        case LEFT_PAREN, RIGHT_PAREN
+        case LEFT_BRACE, RIGHT_BRACE
+        case COMMA
+        case DOT
+        case MINUS
+        case PLUS
+        case SEMICOLON
+        case SLASH
+        case STAR
+        
+        // one or two-character tokens
+        case BANG, BANG_EQUAL
+        case EQUAL, EQUAL_EQUAL
+        case GREATER, GREATER_EQUAL
+        case LESS, LESS_EQUAL
+        
+        // Literals
+        case IDENTIFIER, STRING, NUMBER
+        
+        // Keywords
+        case AND, CLASS, ELSE, FALSE, FUN, FOR, IF, NIL
+        case OR, PRINT, RETURN, SUPER, THIS, TRUE, VAR, WHILE
+        
+        case EOF
+    }
     static func runFile(_ path: String) throws {
 //        byte[] bytes = Files.readAllBytes(Paths.get(path));
 //        run(new String(bytes, Charset.defaultCharset()));
