054b2b960f5b25675a5f700bba5fc08890ef53c0
Joe Heck <heckj@mac.com>
2021-03-23 19:24:44 -0700

    adding tests - incomplete parsing exhibiting

--- Tests/SloxTests/ParserTests.swift
+++ Tests/SloxTests/ParserTests.swift
@@ -2,19 +2,47 @@
 import XCTest
 
 final class ParserTests: XCTestCase {
-    func testTokenizing() {
-        let tokens = Slox.Scanner("1+2*3/4-5;").scanTokens()
-        XCTAssertEqual(tokens.count, 11)
-        XCTAssertEqual(tokens[0].type, TokenType.NUMBER)
-        XCTAssertEqual(tokens[1].type, TokenType.PLUS)
-        XCTAssertEqual(tokens[2].type, TokenType.NUMBER)
-        XCTAssertEqual(tokens[3].type, TokenType.STAR)
-        XCTAssertEqual(tokens[4].type, TokenType.NUMBER)
-        XCTAssertEqual(tokens[5].type, TokenType.SLASH)
-        XCTAssertEqual(tokens[6].type, TokenType.NUMBER)
-        XCTAssertEqual(tokens[7].type, TokenType.MINUS)
-        XCTAssertEqual(tokens[8].type, TokenType.NUMBER)
-        XCTAssertEqual(tokens[9].type, TokenType.SEMICOLON)
-        XCTAssertEqual(tokens[10].type, TokenType.EOF)
+    let source = """
+var foo = 1;
+print foo;
+if (foo > 3) { print "many" };
+// if (foo < 2);
+var pi = 3.14159;
+"""
+
+    func testParser() {
+        let tokenlist = Scanner("1+2*3/4-5;").scanTokens()
+        XCTAssertNotNil(tokenlist)
+        XCTAssertEqual(tokenlist.count, 11)
+
+        let parser = Parser(tokenlist)
+        XCTAssertNotNil(parser)
+        XCTAssertEqual(parser.current, 0)
+        XCTAssertEqual(parser.tokens.count, 11)
+        
+        let statements = parser.parse()
+        
+        XCTAssertEqual(statements.count, 1)
+        XCTAssertEqual(String(describing: statements[0]), "STMT<( - ( + 1.0 ( / ( * 2.0 3.0 ) 4.0 ) ) 5.0 )>")
+    }
+    
+    func testLargerParse() {
+        let tokenlist = Scanner(source).scanTokens()
+        XCTAssertNotNil(tokenlist)
+        XCTAssertEqual(tokenlist.count, 25)
+
+        let parser = Parser(tokenlist)
+        XCTAssertNotNil(parser)
+        XCTAssertEqual(parser.current, 0)
+        XCTAssertEqual(parser.tokens.count, 25)
+        
+        let statements = parser.parse()
+        XCTAssertEqual(parser.current, 24)
+        XCTAssertEqual(parser.tokens.count, 25)
+        print(parser.tokens)
+
+        XCTAssertEqual(statements.count, 2)
+        XCTAssertEqual(String(describing: statements[0]), "VAR(IDENTIFIER foo):(1.0)")
+        XCTAssertEqual(String(describing: statements[1]), "PRINT(var(foo))")
     }
 }
