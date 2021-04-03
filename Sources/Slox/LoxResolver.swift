4ab3cb558081141d0b9c5e0cab8a19613eac0ea5
Joe Heck <heckj@mac.com>
2021-04-03 11:46:28 -0700

    section 11.3.4

--- Sources/Slox/LoxResolver.swift
+++ Sources/Slox/LoxResolver.swift
@@ -14,12 +14,43 @@ public class Resolver {
         self.interpretter = interpretter
     }
     
-    func visit(_ stmt: Statement) {
+    func resolve(_ stmt: Statement) {
         switch stmt {
         case let .block(stmts):
             beginScope()
             resolve(stmts)
             endScope()
+        case let .variable(tok, expr):
+            declare(tok) // declare(stmt.name)
+            switch expr {
+            case .empty:
+                pass()
+            default:
+                expr.resolve(self)
+            }
+        }
+    }
+    
+    func resolve(_ expr: Expression) {
+        switch expr {
+        case let .variable(tok):
+            if !scopes.isEmpty && scopes.last?[tok.lexeme] == false {
+                Lox.report(line: 0, example: tok.lexeme, message: "Can't read local variable in its own initializer.")
+            }
+            resolveLocal(expr, tok)
+        case let .assign(tok, expr):
+            resolve(expr)
+            resolveLocal(expr, tok)
+        }
+        
+    }
+    
+    func resolveLocal(_ expr: Expression, _ name: Token) {
+        //scopes.enumerated() // (index, element)
+        for (idx, scope) in scopes.enumerated().reversed() {
+            if scopes[idx].keys.contains(name.lexeme) {
+                interpretter.resolve(expr, idx)
+            }
         }
     }
     
@@ -30,6 +61,7 @@ public class Resolver {
     func endScope() {
         _ = scopes.popLast()
     }
+    
     private func declare(_ tok: Token) {
         if scopes.isEmpty { return }
         if var scope = scopes.last {
@@ -40,12 +72,14 @@ public class Resolver {
             
         //}
     }
+    
     private func define(_ tok: Token) {
         if scopes.isEmpty { return }
         if var scope = scopes.last {
             scope[tok.lexeme] = true
         }
     }
+    
     func resolve(_ statements: [Statement]) {
         for statement in statements {
             statement.resolve(self)
@@ -61,16 +95,6 @@ func pass() {
 
 extension Statement {
     func resolve(_ resolver: Resolver) {
-        switch self {
-        case let .variable(tok, expr):
-            declare(tok) // declare(stmt.name)
-            switch expr {
-            case .empty:
-                pass()
-            default:
-                expr.resolve(resolver)
-            }
-        }
         
     }
 }
